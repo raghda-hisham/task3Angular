376220b8e32eba1949d517358e459f0917c6d7db
raghda <raghdaelmawi@gmail.com>
2022-05-27 00:49:48 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { SearchBarComponent } from './navbar/search-bar.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { FontAwesomeModule , FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,9 +24,15 @@ import { NgSelectModule } from '@ng-select/ng-select';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    NgSelectModule
+    NgSelectModule,
+    FontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library:FaIconLibrary){
+    library.addIconPacks(fas,far)
+  }
+ }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,41 +1,59 @@
-import { Component, Input, OnInit, Output} from '@angular/core';
+import { Component,EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { MovieServiceService } from '../movie-service.service';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
-import {faHeart as faNotFav ,faEye as faNotWatched} from '@fortawesome/free-regular-svg-icons'
-
+import { Select } from './optionsMovies'
+import { observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @Input() id:number=0;
 
     public sort="";
+    faFav:boolean=false;
     listOfImages:any=[];
     data:any=[];
-    movies:any=[
-      {id:1,name:"top_rated"},
-      {id:2 , name:"upcoming"},
-      {id:3 , name:"popular"}
-    ];
-    moviesCategory:string="top_rated";
-    modifedtext:string="";
+    movies:string='';
+    select:Select[] |any;
+    selectedVal:any;
+    modifedtext:any="top_rated";
+    favMovies:Array<any>=[];
+    favMovie:Array<any>=[];
 
   constructor(public _service:MovieServiceService) {
   }
   ngOnInit(): void {
 
+  this.select=[
+    {id:1,name:"top_rated"},
+    {id:2 , name:"upcoming"},
+    {id:3 , name:"popular"}
+  ]
+   this.getData();
 
-  //   this._service.find(this.moviesCategory).subscribe((res)=>{
-  //     this.data=res.results;
-  // })
   }
 
-  changeFn(e:any){
-    this.movies=e.target.value;
-    console.log(e.target.value)
+  getData(){
+    // this.onSelectedValue;
+    this._service.find(this.modifedtext).subscribe((res)=>{
+      this.data=res.results;
+    })
+  }
+  onSelectedValue(val:any){
+    this.modifedtext= val;
+    this.getData();
+  }
+  onFavClick(obj:object,id:number):void{
+    this.data.filter((item:any)=>{
+      if(item.id === id){
+
+        this.faFav = !this.faFav;
+         this.favMovie.push(JSON.stringify(obj));
+         localStorage.setItem('fav',  JSON.stringify(this.favMovies));
+      }
+    })
   }
 
 
 }
+
